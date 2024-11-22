33b6ff1ba543718a3f08ae4da3080e7fc7fe041b
Austin Hunt <austinhunt818@u.boisestate.edu>
2024-11-22 16:09:44 -0700

    added note names to buttons

--- js/interactive.js
+++ js/interactive.js
@@ -11,18 +11,18 @@ function populateInteractiveGrid() {
     for(var i = 1; i <= 16; i++){
         document.querySelector('.interactiveContainer').innerHTML += `
         <div class="noteColumn" id="note-${i}">
-            <button class="noteButton" id="G5-${i}"></button>
-            <button class="noteButton" id="F5-${i}"></button>
-            <button class="noteButton" id="E5-${i}"></button>
-            <button class="noteButton" id="D5-${i}"></button>
-            <button class="noteButton" id="C5-${i}"></button>
-            <button class="noteButton" id="B4-${i}"></button>
-            <button class="noteButton" id="A4-${i}"></button>
-            <button class="noteButton" id="G4-${i}"></button>
-            <button class="noteButton" id="F4-${i}"></button>
-            <button class="noteButton" id="E4-${i}"></button>
-            <button class="noteButton" id="D4-${i}"></button>
-            <button class="noteButton" id="C4-${i}"></button>
+            <button class="noteButton" id="G5-${i}">G5</button>
+            <button class="noteButton" id="F5-${i}">F5</button>
+            <button class="noteButton" id="E5-${i}">E5</button>
+            <button class="noteButton" id="D5-${i}">D5</button>
+            <button class="noteButton" id="C5-${i}">C5</button>
+            <button class="noteButton" id="B4-${i}">B4</button>
+            <button class="noteButton" id="A4-${i}">A4</button>
+            <button class="noteButton" id="G4-${i}">G4</button>
+            <button class="noteButton" id="F4-${i}">F4</button>
+            <button class="noteButton" id="E4-${i}">E4</button>
+            <button class="noteButton" id="D4-${i}">D4</button>
+            <button class="noteButton" id="C4-${i}">C4</button>
             <p class="noteLabel"> </p>
       </div>
         `;
@@ -34,16 +34,3 @@ document.querySelector('.interactiveContainer').addEventListener('click', functi
         piano.start({ note: e.target.id.substring(0,2)})
     }
 });
-
-// document.getElementById('noteG5').addEventListener('click', ()=>piano.start({ note: 'G5'}));
-// document.getElementById('noteF5').addEventListener('click', ()=>piano.start({ note: 'F5'}));
-// document.getElementById('noteE5').addEventListener('click', ()=>piano.start({ note: 'E5'}));
-// document.getElementById('noteD5').addEventListener('click', ()=>piano.start({ note: 'D5'}));
-// document.getElementById('noteC5').addEventListener('click', ()=>piano.start({ note: 'C5'}));
-// document.getElementById('noteB4').addEventListener('click', ()=>piano.start({ note: 'B4'}));
-// document.getElementById('noteA4').addEventListener('click', ()=>piano.start({ note: 'A4'}));
-// document.getElementById('noteG4').addEventListener('click', ()=>piano.start({ note: 'G4'}));
-// document.getElementById('noteF4').addEventListener('click', ()=>piano.start({ note: 'F4'}));
-// document.getElementById('noteE4').addEventListener('click', ()=>piano.start({ note: 'E4'}));
-// document.getElementById('noteD4').addEventListener('click', ()=>piano.start({ note: 'D4'}));
-// document.getElementById('noteC4').addEventListener('click', ()=>piano.start({ note: 'C4'}));
